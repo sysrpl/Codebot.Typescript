2559b8b3478a38875e5ff6cfc3cae214164bf9f3
Anthony Walter <sysrpl@gmail.com>
2020-06-10 20:46:45 -0400

    Changed init.sh

--- init.sh
+++ init.sh
@@ -3,4 +3,3 @@
 git update-index --assume-unchanged make.sh
 git update-index --assume-unchanged .vscode/tasks.json
 git update-index --assume-unchanged source/app/app.ts
-git update-index --assume-unchanged tsconfig.json
